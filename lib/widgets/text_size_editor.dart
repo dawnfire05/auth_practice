574c15f5d73c9b7e1ad8b58b120f382fde712ade
dawnfire05 <dawnfire05@naver.com>
2024-04-24 03:32:46 +0900

    modified:   lib/main.dart 	modified:   lib/widgets/text_size_editor.dart

--- lib/widgets/text_size_editor.dart
+++ lib/widgets/text_size_editor.dart
@@ -12,20 +12,22 @@ class _TextSizeEditorState extends State<TextSizeEditor> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         children: [
-          Text("텍스트 사이즈를 조절하세요"),
+          const Text("텍스트 사이즈를 조절하세요"),
           Slider(
             value: _currentNum,
             max: 100,
             divisions: 5,
             onChanged: (double value) {
-              setState((){
-                _currentNum = value;
-              });
-            }),
-          )
+              setState(
+                () {
+                  _currentNum = value;
+                },
+              );
+            },
+          ),
         ],
       ),
     );
